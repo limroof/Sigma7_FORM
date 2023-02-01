219c018b11ecd113393e0661022395fd7f1d4b99
limroof <ismail.elhadaj@gmail.com>
2023-01-31 21:57:49 -0500

    first-commit

--- Controllers/sendEmail.js
+++ Controllers/sendEmail.js
@@ -19,7 +19,16 @@ exports.sendEmail = (req, res) => {
     to: req.body.email,
     subject: "Sending Email using Node.js",
     text: req.body.message,
-    html: "<h1>Welcome</h1><p>That was easy!</p>",
+    html:
+      "<h1>Confirmation de réception</h1><p>Ceci est un courriel pour vous confimer que nous avons bien reçu votre message</p><p><strong>Voici les informations fournis :</strong><br/><strong>Nom : " +
+      req.body.name +
+      "</strong></br><strong>Numéro : " +
+      req.body.telephone +
+      "</strong></br><strong>E-mail : " +
+      req.body.email +
+      "</strong></br><strong>message : " +
+      req.body.message +
+      "</strong></br></p>",
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
