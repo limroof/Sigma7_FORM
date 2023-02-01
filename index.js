1f10dfe7aa5fb6aea4d0360990530fd58a30c45f
limroof <ismail.elhadaj@gmail.com>
2023-01-31 21:20:56 -0500

    first commit

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ var bodyParser = require("body-parser");
 var app = express();
 var cors = require("cors");
 
-const thematiqueRouter = require("./Routes/EmailRoute");
+const sendEmailRouter = require("./Routes/EmailRoute");
 
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV == "DEVELOPMENT"
+        ? "http://localhost:3000"
+        : "https://sigma7.online,https://sigma7.ma",
+  })
+);
 app.use(bodyParser.json());
 /* app.use(express.urlencoded()); */
 
-app.use("/send", thematiqueRouter);
+app.use("/send", sendEmailRouter);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
