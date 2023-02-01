106ab495a98af737be811ed7e53ce4e4917aff05
limroof <ismail.elhadaj@gmail.com>
2023-02-01 03:32:54 -0500

    first-commit

--- Controllers/sendEmail.js
+++ Controllers/sendEmail.js
@@ -20,7 +20,7 @@ exports.sendEmail = (req, res) => {
     subject: "Confirmation de message",
     text: req.body.message,
     html:
-      "<h1>Confirmation de réception</h1><p>Ceci est un courriel pour vous confimer que nous avons bien reçu votre message</p><p><strong>Voici les informations fournis :</strong><br/><strong>Nom : " +
+      "<h1>Confirmation de réception</h1><p>Ceci est un courriel pour vous confimer que nous avons bien reçu votre message</p><p><strong>Voici les informations fournies :</strong><br/><strong>Nom : " +
       req.body.name +
       "</strong><br><strong>Numéro : " +
       req.body.telephone +
